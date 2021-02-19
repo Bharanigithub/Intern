59639f42c5c70c92688402546765fd4bba892245
Bharanigithub <69192638+Bharanigithub@users.noreply.github.com>
2021-02-19 22:51:02 +0530

    Add files via upload

---
+++ Pgm3.java
@@ -0,0 +1,22 @@
+package Day1;
+import java.util.*;
+public class Pgm3 {
+	public static void main(String args[]) {
+		char c='c';
+		byte b=2;
+		short s=10;
+		int i=65482;
+		long l=48754562;
+		float f=2.3f;
+		double d=58563.25;
+		boolean bo=true;
+		int ii=c;
+		long li=i;
+		int ie=(int)l;
+		char ce=(char)48;
+		System.out.println(ii);
+		System.out.println(li);
+		System.out.println(ie);
+		System.out.println(ce);
+	}
+}
