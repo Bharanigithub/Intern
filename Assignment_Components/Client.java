d7de2961bada44c4cf1e92c214cc1f14bb323b88
Bharanigithub <69192638+Bharanigithub@users.noreply.github.com>
2021-03-15 19:22:41 +0530

    Update Client.java

--- Assignment_Components/Client.java
+++ Assignment_Components/Client.java
@@ -1,36 +1,84 @@
-package day16;
+package Assignment_Components;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.*;
 
+public class Server {
+	int port;
+	ServerSocket server = null;
+	Socket client = null;
+	ExecutorService pool = null;
+	int noOfClient = 0;
 
-public class Client {
-	Socket s;
-	BufferedReader br,in;
-	PrintWriter out;
-	public Client() {
-		// TODO Auto-generated constructor stub
-		try {
-			while(true) { 
-				s=new Socket("localhost",2000);
-				br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				String msgFromServer = br.readLine();
-				System.out.println("Message From Server...:" + msgFromServer);
-				out = new PrintWriter(s.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("please enter a message for Server...:");
-				String msg = in.readLine();
-				out.println(msg);
+	public static void main(String[] args) throws Exception {
+		Server serverObject = new Server();
+		serverObject.startServer();
+	}
+
+	public Server() {
+		pool = Executors.newFixedThreadPool(5);
+	}
+
+	public void startServer() throws Exception {
+		server = new ServerSocket(8000);
+		System.out.println("Server Started!!");
+		while (true) {
+			client = server.accept();
+			noOfClient++;
+			ServerThread run = new ServerThread(client, noOfClient, this);
+			pool.execute(run);
+		}
+	}
+
+	private static class ServerThread implements Runnable {
+		Server server = null;
+		Socket client = null;
+		BufferedReader in;
+		PrintStream out;
+		int id;
+		String message;
+		Scanner sc = new Scanner(System.in);
+
+		public ServerThread(Socket client, int noOfClient, Server server) throws Exception {
+			this.client = client;
+			this.server = server;
+			this.id = noOfClient;
+
+			System.out.println("client "+id+" Conneceted!!" );
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			out = new PrintStream(client.getOutputStream());
+		}
+
+		@Override
+		public void run() {
+			boolean status = true;
+			try {
+				while (true) {
+					message = in.readLine();
+					System.out.println("From Client " + id + ":" + message);
+					System.out.println();
+					message = sc.nextLine();
+					if (message.equalsIgnoreCase("exit")) {
+						out.println("exited....");
+						status = false;
+						break;
+					}
+					out.println(message);
+				}
+				in.close();
+				out.close();
+				client.close();
+				if (status==false) {
+					System.exit(0);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
 	}
-	public static void main(String[] args) {
-		new Client();
-	}
+
 }
