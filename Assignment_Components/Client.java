cfc94370b996ac9ce36b9ac2614b173524bad3e3
Bharanigithub <69192638+Bharanigithub@users.noreply.github.com>
2021-03-15 19:23:14 +0530

    Update Client.java

--- Assignment_Components/Client.java
+++ Assignment_Components/Client.java
@@ -1,84 +1,32 @@
 package Assignment_Components;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.io.*;
+import java.net.Socket;
 
-public class Server {
-	int port;
-	ServerSocket server = null;
-	Socket client = null;
-	ExecutorService pool = null;
-	int noOfClient = 0;
-
-	public static void main(String[] args) throws Exception {
-		Server serverObject = new Server();
-		serverObject.startServer();
-	}
-
-	public Server() {
-		pool = Executors.newFixedThreadPool(5);
-	}
-
-	public void startServer() throws Exception {
-		server = new ServerSocket(8000);
-		System.out.println("Server Started!!");
-		while (true) {
-			client = server.accept();
-			noOfClient++;
-			ServerThread run = new ServerThread(client, noOfClient, this);
-			pool.execute(run);
-		}
-	}
-
-	private static class ServerThread implements Runnable {
-		Server server = null;
-		Socket client = null;
-		BufferedReader in;
-		PrintStream out;
-		int id;
+public class Client {
+	public static void main(String[] args) throws IOException {
+		Socket socket = new Socket("localhost", 8000);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		String message;
-		Scanner sc = new Scanner(System.in);
 
-		public ServerThread(Socket client, int noOfClient, Server server) throws Exception {
-			this.client = client;
-			this.server = server;
-			this.id = noOfClient;
-
-			System.out.println("client "+id+" Conneceted!!" );
-			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			out = new PrintStream(client.getOutputStream());
-		}
-
-		@Override
-		public void run() {
-			boolean status = true;
-			try {
-				while (true) {
-					message = in.readLine();
-					System.out.println("From Client " + id + ":" + message);
-					System.out.println();
-					message = sc.nextLine();
-					if (message.equalsIgnoreCase("exit")) {
-						out.println("exited....");
-						status = false;
-						break;
-					}
-					out.println(message);
-				}
-				in.close();
-				out.close();
-				client.close();
-				if (status==false) {
-					System.exit(0);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+		while (true) {
+			System.out.println("Client : ");
+			message = input.readLine();
+			out.println(message);
+			if (message.equalsIgnoreCase("exit")) {
+				System.out.println("Exit!!!");
+				break;
 			}
+			message = in.readLine();
+			System.out.println("Server says, " + message);
+			System.out.println();
 		}
-	}
 
+		socket.close();
+		in.close();
+		out.close();
+		input.close();
+	}
 }
